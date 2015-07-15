20c3d31fc736108e7643d4fcf199031576fe848b
NaiveRed <jason841201@gmail.com>
2015-07-15 19:29:18 +0800

    0715 update
    delete the folder and upload 10018#

---
+++ 10018# - Reverse and Add.cpp
@@ -0,0 +1,46 @@
+#include<stdio.h>
+
+long long re(long long n);//將數字顛倒
+
+int main(){
+
+	int set;
+	long long num, temp;
+
+	while (scanf("%d", &set) != EOF)
+	{
+		for (; set > 0; set--)
+		{
+			scanf("%lld", &num);
+
+			int count = 0;
+
+
+			while ((temp = re(num)) != num)//將數字顛倒後，判斷是否迴文
+			{
+				num += temp;//原本的加上顛倒後的成為新的數字
+				count++;
+
+			}
+
+			printf("%d %lld\n", count, temp);
+		}
+
+	}
+	return 0;
+}
+
+long long re(long long n)
+{
+	long long temp = 0;
+
+
+	while (n)
+	{
+		temp = temp * 10 + n % 10;
+		n /= 10;
+
+	}
+	return temp;
+
+}
--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 The practicing of UVa Online Judge.(HungChungChun)
 
-主要是拿來存放練習的程式碼。
+主要是拿來存放練習的程式碼，如果有 # 的符號是比較久前寫的，寫法可能比較差!
 
 一些之前寫的題目在: [Blog](http://naivered.blogspot.tw/)
